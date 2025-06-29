a3ddbf24656756d011333eb8dd172df5c1d10ae0
avazbek <avazbek@gmail.com>
2025-06-29 11:35:44 +0500

    ..

--- js/app.js
+++ js/app.js
@@ -61,10 +61,10 @@ function play() {
   playBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`;
 
   clearInterval(bgColorInterval);
-
   bgColorInterval = setInterval(() => {
-    document.body.style.backgroundColor = getRandomColor();
+    document.body.style.backgroundColor = getRandomRGBAColor();
   }, 1000);
+
 }
 
 function nextSong() {
@@ -167,14 +167,15 @@ speedMenu.addEventListener("click", (e) => {
 
 let bgColorInterval;
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getRandomRGBAColor() {
+  const r = Math.floor(Math.random() * 256);   
+  const g = Math.floor(Math.random() * 256);   
+  const b = Math.floor(Math.random() * 256);   
+  const a = 0.4; 
+
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
+
 
 playBtn.addEventListener("click", muzicPlay);
 audio.addEventListener("timeupdate", progress);
